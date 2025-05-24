382f463cbfce8a6de5c57cfead8875fb1c357154
unknown <brandontweaks11@gmail.com>
2025-05-24 15:10:25 -0600

    version 0.0.1

---
+++ src/main/java/com/pia/busplus/BusplusApplication.java
@@ -0,0 +1,13 @@
+package com.pia.busplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusplusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pia/busplus/BusplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pia.busplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
